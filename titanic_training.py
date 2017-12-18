e4c4806f3b587a9af091e038cca374cefcad4590
Calin Radu Calin <crcalin@gmail.com>
2017-12-18 18:12:31 +0200

    basic data exploring & visualization

--- titanic_training.py
+++ titanic_training.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import svm
+from sklearn import model_selection as ms
 
 # Config
 pd.options.mode.chained_assignment = None
@@ -9,7 +11,55 @@ pd.options.mode.chained_assignment = None
 train = pd.read_csv('train.csv')
 test = pd.read_csv('test.csv')
 
-#CLEANING DATA
+
+def explore_data():
+
+    print("Train Data Preview")
+    print(train.head()) # prints first 5 rows of training data set
+
+    print("\nTrain vs Test Data")
+    print(train.shape) # 891 entries, 12 features
+    print(test.shape) # 418 entries, 11 features
+
+    print("\nTrain Data Info")
+    print(train.info())
+    print(train.isnull().sum()) # -> Missing values: Age(177), Cabin(687), Embarked(2)
+    print("\nMissing Values")
+
+    print("\nTest Data Info")
+    print(test.info())
+    print("\nMissing Values")
+    print(test.isnull().sum()) # -> Missing values: Age(86), Fare(1), Cabin(327)
+
+
+def bar_chart(feature):
+
+    survived = train[train['Survived'] == 1][feature].value_counts()
+    dead = train[train['Survived'] == 0][feature].value_counts()
+    df = pd.DataFrame([survived, dead])
+    df.index = ['Survived', 'Dead']
+    df.plot(kind='bar', stacked=True, figsize=(10, 5), title=feature)
+
+
+def visualize_data():
+
+    bar_chart("Sex")        # Women -> more chance to survive
+    bar_chart("Age")        # Too many values
+    bar_chart("Pclass")     # 1st class -> more chance to survive; 3rd class -> more chance to die
+    bar_chart("SibSp")      # If you have 1 Sibling or Spouse -> more chance to survive
+    bar_chart('Parch')      # If you have 1 Parent/Child -> more chance to survive
+    bar_chart('Embarked')   # C -> maybe more chance to survive?
+
+    plt.show()
+
+
+
+# explore_data()
+# visualize_data()
+
+
+
+'''
 
 # Update missing age with median value
 median_age = train["Age"].median()
@@ -44,17 +94,21 @@ test["Embarked"][test["Embarked"] == "Q"] = 2
 # TRAIN using SVM - Gaussian Kernel (Radial basis function)
 svm_gaussian = svm.SVC(kernel="rbf")
 
-X_train = train[["Pclass", "Age", "SibSp", "Parch", "Child", "Embarked"]]
-y_train = train["Survived"]
+X = train[["Pclass", "Age", "SibSp", "Parch", "Child", "Embarked"]]
+y = train["Survived"]
 
-X_test = test[["Pclass", "Age", "SibSp", "Parch", "Child", "Embarked"]]
+test_features = test[["Pclass", "Age", "SibSp", "Parch", "Child", "Embarked"]]
 
-svm_gaussian.fit(X_train, y_train)
-y_test = svm_gaussian.predict(X_test)
+X_train, X_test, y_train, y_test = ms.train_test_split(X, y, test_size=0.3, random_state=0)
 
+classifier = svm_gaussian.fit(X_train, y_train)
+cv_score = classifier.score(X_test, y_test)
+print(cv_score)
 
 # Write output
 # Create a data frame with two columns: PassengerId & Survived
 PassengerId = np.array(test["PassengerId"]).astype(int)
 my_solution = pd.DataFrame(y_test, PassengerId, columns=["Survived"])
 prediction_file = my_solution.to_csv(path_or_buf="prediction.csv", index_label="PassengerId")
+
+'''
